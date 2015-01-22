1d521a8fc5aa60492c73637f77d1a6148adcb550
Hackbright Student <info@hackbrightacademy.com>
2015-01-22 13:08:41 -0800

    Wrote make_text function

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from random import choice
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
@@ -19,20 +20,28 @@ def make_chains(corpus):
         new_dict[key].append(final_list[index + 2])
     return new_dict
 
-# def make_text(chains):
-#     """Takes a dictionary of markov chains and returns random text
-#     based off an original text."""
-#     return "Here's some random text."
+def make_text(chains):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    first_key = choice(chains.keys())
+    first_value = choice(chains[first_key])
+    new_string = first_key[0] + " " + first_key[1]
+    next_key = (first_key[1], first_value)
+
+    while next_key in chains:
+        current_value = choice(chains[next_key])
+        new_string = new_string + " " + next_key[1]
+        next_key = (next_key[1], current_value)
+
+    return new_string + " " + current_value
 
 def main():
     args = sys.argv
     script, path = args
     file_to_parse = open(path)
-  
     chain_dict = make_chains(file_to_parse)
-    print chain_dict
-    # random_text = make_text(chain_dict)
-    # print random_text
+    random_text = make_text(chain_dict)
+    print random_text
 
 if __name__ == "__main__":
     main()
